484ac48aa3d300e0ef5e9a9b7a036559a71a5121
outrest <floem@web.de>
2021-05-31 19:10:05 +0200

    fast am ziel

--- src/Aufgabe2.java
+++ src/Aufgabe2.java
@@ -1,4 +1,6 @@
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
 import java.nio.Buffer;
@@ -10,7 +12,7 @@ public class Aufgabe2 {
     public static void main(String[] args) throws IOException, InvalidKeyException {
 
         byte[] key_byte = {0x00, 0x00, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55};
-        byte[] iv = {(byte) 0x80, (byte) 0x81, (byte) 0x82, (byte) 0x83, (byte) 0x84, (byte) 0x85, (byte) 0x86, (byte) 0x87, (byte) 0x88, (byte) 0x89, (byte) 0x8a, (byte) 0x8b, (byte) 0x8c, (byte) 0x8d, (byte) 0x8f};
+        byte[] iv = {(byte) 0x80, (byte) 0x81, (byte) 0x82, (byte) 0x83, (byte) 0x84, (byte) 0x85, (byte) 0x86, (byte) 0x87, (byte) 0x88, (byte) 0x89, (byte) 0x8a, (byte) 0x8b, (byte) 0x8c, (byte) 0x8d, (byte) 0x8e, (byte) 0x8f};
 
 
         ByteArrayInputStream br = new ByteArrayInputStream(new FileInputStream("src\\chiffrat_AES.bin").readAllBytes());
@@ -21,33 +23,39 @@ public class Aufgabe2 {
             Cipher aes = Cipher.getInstance("AES/CBC/PKCS5Padding");
             SecretKeySpec key;
             byte[] AES_byte;
-
+            IvParameterSpec invec = new IvParameterSpec(iv);
             for (int i = 0; i < 256; i++) {
+                key_byte[0] = (byte) i;
                 for (int j = 0; j < 256; j++) {
-                    key_byte[0] = (byte) i;
                     key_byte[1] = (byte) j;
                     key = new SecretKeySpec(key_byte, "AES");
-                    aes.init(Cipher.DECRYPT_MODE, key);
-                    AES_byte = aes.doFinal(bsr.readAllBytes());
+                    aes.init(Cipher.DECRYPT_MODE, key, invec);
+                    try {
+                        AES_byte = aes.doFinal(bsr.readAllBytes());
+                    } catch (BadPaddingException e) {
+                        AES_byte = new byte[]{0, 0, 0, 0};
+                        continue;
+                    }
                     if (checkMyBeginning(AES_byte)) {
                         System.out.println("Key: ");
                         for (byte etwas : AES_byte) {
                             System.out.print(String.format("%x", Byte.toUnsignedInt(etwas)) + " ");
                         }
-
                         break;
                     }
                 }
             }
 
         } catch (Exception e) {
-            System.out.println(e);
+           e.printStackTrace();
         }
 
     }
 
     //%pdf
     private static boolean checkMyBeginning(byte[] lol) {
-        return ((lol[0] == 37) && (lol[1] == 112) && (lol[2] == 100) && (lol[3] == 102));
+        if (lol==null||lol.length==0)
+            return false;
+        return ((lol[0] == 0x25) && (lol[1] == 0x50) && (lol[2] == 0x44) && (lol[3] == 0x46));
     }
 }
